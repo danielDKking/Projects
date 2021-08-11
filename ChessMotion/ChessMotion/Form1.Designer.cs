749a28c159d771b3643c1803c04c9af235bce581
danielDKking <88778499+danielDKking@users.noreply.github.com>
2021-08-11 19:01:37 +0700

    Add files via upload

---
+++ ChessMotion/ChessMotion/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace ChessMotion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_Letter = new System.Windows.Forms.ComboBox();
+            this.cb_Number = new System.Windows.Forms.ComboBox();
+            this.btn_ShowAvailableCells = new System.Windows.Forms.Button();
+            this.tb_ListAvailableCells = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Начальное полодение:";
+            // 
+            // cb_Letter
+            // 
+            this.cb_Letter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_Letter.FormattingEnabled = true;
+            this.cb_Letter.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H"});
+            this.cb_Letter.Location = new System.Drawing.Point(15, 25);
+            this.cb_Letter.Name = "cb_Letter";
+            this.cb_Letter.Size = new System.Drawing.Size(47, 21);
+            this.cb_Letter.TabIndex = 1;
+            // 
+            // cb_Number
+            // 
+            this.cb_Number.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_Number.FormattingEnabled = true;
+            this.cb_Number.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.cb_Number.Location = new System.Drawing.Point(68, 25);
+            this.cb_Number.Name = "cb_Number";
+            this.cb_Number.Size = new System.Drawing.Size(47, 21);
+            this.cb_Number.TabIndex = 2;
+            // 
+            // btn_ShowAvailableCells
+            // 
+            this.btn_ShowAvailableCells.Location = new System.Drawing.Point(15, 63);
+            this.btn_ShowAvailableCells.Name = "btn_ShowAvailableCells";
+            this.btn_ShowAvailableCells.Size = new System.Drawing.Size(119, 23);
+            this.btn_ShowAvailableCells.TabIndex = 3;
+            this.btn_ShowAvailableCells.Text = "Доступные ходы";
+            this.btn_ShowAvailableCells.UseVisualStyleBackColor = true;
+            this.btn_ShowAvailableCells.Click += new System.EventHandler(this.btn_ShowAvailableCells_Click);
+            // 
+            // tb_ListAvailableCells
+            // 
+            this.tb_ListAvailableCells.Location = new System.Drawing.Point(15, 92);
+            this.tb_ListAvailableCells.Multiline = true;
+            this.tb_ListAvailableCells.Name = "tb_ListAvailableCells";
+            this.tb_ListAvailableCells.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_ListAvailableCells.Size = new System.Drawing.Size(119, 346);
+            this.tb_ListAvailableCells.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(149, 450);
+            this.Controls.Add(this.tb_ListAvailableCells);
+            this.Controls.Add(this.btn_ShowAvailableCells);
+            this.Controls.Add(this.cb_Number);
+            this.Controls.Add(this.cb_Letter);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cb_Letter;
+        private System.Windows.Forms.ComboBox cb_Number;
+        private System.Windows.Forms.Button btn_ShowAvailableCells;
+        private System.Windows.Forms.TextBox tb_ListAvailableCells;
+    }
+}
+
---
+++ ChessMotion/ChessMotion/Form1.cs
@@ -0,0 +1,92 @@
+﻿using DocumentFormat.OpenXml.Bibliography;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessMotion
+{
+    public partial class Form1 : Form
+    {
+        private List<char> ListAvailableLetters = new List<char> { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
+        private List<int> ListAvailableNumbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            cb_Number.SelectedIndex = 0;
+            cb_Letter.SelectedIndex = 0;
+        }
+
+        private void btn_ShowAvailableCells_Click(object sender, EventArgs e)
+        {
+            char startLetter = cb_Letter.Items[cb_Letter.SelectedIndex].ToString().First();
+            int startNumber = Convert.ToInt32(cb_Number.Items[cb_Number.SelectedIndex].ToString());
+
+            List<(char letter, int number)> ListAvailableCells = GetAvailableCellsForQueen(startLetter, startNumber);
+
+            tb_ListAvailableCells.Text = "";
+            ListAvailableCells.ForEach(x => tb_ListAvailableCells.Text += "(" + x.letter + ";" + x.number + ")" + ";" + Environment.NewLine);
+        }
+
+        private List<(char letter, int number)> GetAvailableCellsForQueen(char startLetter, int startNumber) 
+        {
+            List<(char letter, int number)> ListAvailableCells = new List<(char letter, int number)>();
+
+            // Получить все возможные вертикальные клетки
+            foreach (int number in ListAvailableNumbers) 
+            {
+                // Исключение номера текущей позиции
+                if (number == startNumber)
+                    continue;
+
+                ListAvailableCells.Add((startLetter, number));
+            }
+            // Получить все возможные горизнтальные клетки
+            foreach (char letter in ListAvailableLetters)
+            {
+                // Исключение буквы текущей позиции
+                if (letter == startLetter)
+                    continue;
+
+                ListAvailableCells.Add((letter, startNumber));
+            }
+
+            int y = ListAvailableLetters.IndexOf(startLetter) + 1; // координата символа доски
+            int offsetY = y - startNumber; // Отклонение У от главной диагонали
+            // Получить все возможные ходы по главной диагонали
+            for (int i = 1; i <= 8; i++) 
+            {
+                // Исключение индексов за пределами границ массива и индекса текущей позиции
+                if (i + offsetY - 1 < 0 || i + offsetY - 1 > 7 || i == startNumber)
+                    continue;
+
+                ListAvailableCells.Add((ListAvailableLetters[i + offsetY - 1], i));
+            }
+
+            offsetY = y - (8 - startNumber + 1); // Отклонение У от побочной диагонали
+            int d = 1;
+            // Получить все возможные ходы по побочной диагонали
+            for (int i = 8; i >= 1; i--)
+            {
+                // Исключение индексов за пределами границ массива и индекса текущей позиции
+                if (i + offsetY - 1 < 0 || i - i + offsetY - 1 > 7 || d == startNumber) 
+                {
+                    d++;
+                    continue;
+                }
+                    
+                ListAvailableCells.Add((ListAvailableLetters[i + offsetY - 1], d));
+                d++;
+            }
+
+            return ListAvailableCells;
+        }
+    }
+}
